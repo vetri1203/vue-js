02e0a8efa1121b57b3fda347fcb95d6f0a763734
vetri1203 <vetriveljaganathan5125@gmail.com>
2024-01-23 15:13:33 +0530

    vue-cli initial setup

---
+++ Day-2/demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <h2>{{ hello }}</h2> -->
+  <MyTemp data="vue hii"  :hello="hello"/>
+</template>
+
+<script>
+
+import MyTemp from './components/MyTemp.vue';
+export default {
+  name: "App",
+  components: {MyTemp},
+  data() {
+    return {
+      hello: "Hello form vue",
+    };
+  },
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Day-2/demo/src/components/MyTemp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Hello hii
+
+        <h1>{{ hello }}</h1>
+        <h2>{{ data }}</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "MyTemp",
+    props: {
+        data:{},
+        hello: {
+            type: String
+        }
+    }
+
+
+}
+
+</script>
